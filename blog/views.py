e544506c94e54260c28422e382576be9c9f69600
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-12-03 16:12:03 -0800

    update migrations folder and media

--- blog/views.py
+++ blog/views.py
@@ -4,21 +4,6 @@ from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'John Smith',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
 
 def home(request):
     context = {
